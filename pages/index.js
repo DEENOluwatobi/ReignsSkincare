8d68cd2f803ddeb11e618ea920102e00a8e5e7b1
DEENOluwatobi <shodimutobie@gmail.com>
2023-02-16 13:25:25 +0100

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+      <div>
+        <Head>
+          <title>Reign's Skincare</title>
+          <meta name="description" content="Reign's Skincare" />
+          <link rel="icon" href="/Logo.png" />
+        </Head>
+        {/* body */}
+        <main>
+          <h1>Peep the light show</h1>        
+        </main>
+      </div>
+  );
+}
+
