a2c620cf78be5e01c16d8661c923ec7be410bcf1
DEENOluwatobi <shodimutobie@gmail.com>
2023-02-19 13:06:31 +0100

    Header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,13 +1,30 @@
 import css from '../styles/Header.module.css'
 import Image from 'next/image'
 import Logo from '../assets/Logo.png'
+import { UilShoppingBag } from '@iconscout/react-unicons'
 
 export default function Header() {
     return(
         <div className={css.header}>
             {/* logo */}
             <div className={css.logo}>
-                <Image src={Logo} alt=''/>
+                <Image src={Logo} alt='' width={50} height={50}/>
+                <span>Reign's<br/>Skincare</span>
+            </div>
+
+            {/* menu */}
+            <ul className={css.menu}>
+                <li>Home</li>
+                <li>Menu</li>
+                <li>Contact</li>
+            </ul>
+
+            {/* cart side */}
+            <div className={css.rightside}>
+                <div className={css.cart}>
+                    <UilShoppingBag size={30} color='#2E2E2E'/>
+                    <div className={css.badge}>1</div>
+                </div>
             </div>
         </div>
     )
